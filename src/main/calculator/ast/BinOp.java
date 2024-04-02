f6c0db4adfd5605fb530b973f7966449e5ff245f
v <vova2341591@gmail.com>
2024-04-03 02:50:52 +0300

    aaaaa

--- src/main/calculator/ast/BinOp.java
+++ src/main/calculator/ast/BinOp.java
@@ -5,8 +5,6 @@ public abstract class BinOp implements AST {
     AST left;
     AST right;
 
-    protected static String symbol;
-
     public BinOp(AST left, AST right) {
         this.left = left;
         this.right = right;
@@ -16,17 +14,5 @@ public abstract class BinOp implements AST {
         return String.format("BinOp {\n  left {\n  %s\n}\n  right {\n  %s\n}\n}",left, right);
     }
 
-    @Override
-    public String getStringRepresentation() {
-        StringBuilder b = new StringBuilder();
-        b.append('(');
-        left.getStringRepresentation();
-        b.append(')');
-        b.append(symbol);
-        b.append('(');
-        right.getStringRepresentation();
-        b.append(')');
-        return b.toString();
-    }
 }
 
